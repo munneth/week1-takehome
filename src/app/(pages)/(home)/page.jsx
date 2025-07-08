a4289dd088b3ac5019827c70fd23e64c1dcedf2b
munneth <mugill@ucdavis.edu>
2025-07-08 14:52:02 -0700

    Refactor home page to use RecentWorkComponent and update styles for recent work card

--- src/app/(pages)/(home)/page.jsx
+++ src/app/(pages)/(home)/page.jsx
@@ -1,17 +1,11 @@
 import Image from "next/image";
 import styles from "../_globals/mixins.scss";
-import RecentWorkCard from "../_components/recentWorkCard";
+import RecentWorkComponent from "../_components/recentWorkComponent";
 
 export default function Home() {
   return (
     <>
-      <RecentWorkCard
-        quarter="Q1 2025"
-        imgPath="/images/recentWork/q1.png"
-        title="Recent Work"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="https://www.google.com"
-      />
+      <RecentWorkComponent />
     </>
   );
 }
