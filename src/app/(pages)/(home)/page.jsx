303e66089d0aa8ca2dec03e11da002d2eaf909bf
munneth <mugill@ucdavis.edu>
2025-07-09 11:56:44 -0700

    Refactor home page layout and styles: introduce new top and bottom sections, update mission statement, and enhance visual elements with improved styling

--- src/app/(pages)/(home)/page.jsx
+++ src/app/(pages)/(home)/page.jsx
@@ -1,79 +1,47 @@
+import styles from "./page.module.scss";
 import Image from "next/image";
-import styles from "../_globals/mixins.scss";
-
 import RecentWorkComponent from "../_components/recentWorkComponent";
 
 export default function Home() {
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div>
-          <Image src="/pen.svg" alt="hero" width={50} height={50} />
-        </div>
-        <div>
-          <h1
-            style={{
-              fontFamily: "Inter !important",
-              fontWeight: 700,
-              fontStyle: "bold",
-              fontSize: "3.25rem",
-              lineHeight: "3.625rem",
-              letterSpacing: "0%",
-              verticalAlign: "middle",
-              marginBottom: "3rem",
-            }}
-          >
-            Recent Work
-          </h1>
-        </div>
-        <div>
-          <p
-            style={{
-              fontFamily: "Inter",
-              fontWeight: 400,
-              fontStyle: "normal",
-              fontSize: "2rem",
-              lineHeight: "100%",
-              letterSpacing: "0%",
-              verticalAlign: "middle",
-              marginBottom: "2rem",
-            }}
-          >
-            Transforming ideas into incredible realities is what we do best
-          </p>
-        </div>
+    <div>
+      {/* Top Section (white) */}
+      <div className={styles.topSection}>
+        <h1 className={styles.mission}>
+          Our mission is to design creative
+          <br />
+          digital solutions that have a<br />
+          positive social impact
+        </h1>
       </div>
-      <div>
+      {/* Bottom Section (purple) */}
+      <div className={styles.bottomSection}>
+        <div className={styles.heroImageWrapper}>
+          <Image
+            src="/Frame 1749.png"
+            alt="Hero"
+            width={600}
+            height={250}
+            className={styles.heroImage}
+            priority
+          />
+        </div>
+        {/* Pen icon above Recent Work */}
+        <div className={styles.penWrapper}>
+          <Image
+            src="/Pen.svg"
+            alt="Pen Icon"
+            width={80}
+            height={80}
+            className={styles.pen}
+          />
+        </div>
+        <h3 className={styles.subheading}>Recent Work</h3>
+        <p className={styles.subtitle}>
+          Transforming ideas into incredible realities is what we do best
+        </p>
         <RecentWorkComponent />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p
-          style={{
-            fontFamily: "Inter",
-            fontWeight: 400,
-            fontStyle: "normal",
-            fontSize: "1.125rem",
-            lineHeight: "100%",
-            letterSpacing: "0%",
-            marginTop: "2rem",
-          }}
-        >
-          AND MORE...
-        </p>
-      </div>
-    </>
+    </div>
   );
 }
